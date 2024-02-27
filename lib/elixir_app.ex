7b431ee75f828ff90d53956dff103db88cbaa9d7
Edgar Tolete <edgartolete@outlook.com>
2024-02-27 19:10:38 +0800

    modules

--- lib/elixir_app.ex
+++ lib/elixir_app.ex
@@ -1,18 +1,12 @@
 defmodule ElixirApp do
-  @moduledoc """
-  Documentation for `ElixirApp`.
-  """
+  use Application
 
-  @doc """
-  Hello world.
+  def start(_type, _args) do
+    IO.puts(ElixirApp.hello())
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
 
-  ## Examples
-
-      iex> ElixirApp.hello()
-      :world
-
-  """
   def hello do
-    :world
+    IO.puts("Test2")
   end
 end
