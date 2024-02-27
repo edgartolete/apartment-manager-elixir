00a227b0707422163ffaf247e857afd1de6b12cb
Edgar Tolete <edgartolete@outlook.com>
2024-02-27 19:37:38 +0800

    on progress

--- lib/elixir_app.ex
+++ lib/elixir_app.ex
@@ -1,12 +1,14 @@
 defmodule ElixirApp do
   use Application
-
+  alias UUID
+  @x 5
   def start(_type, _args) do
-    IO.puts(ElixirApp.hello())
+    ElixirApp.main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
-
-  def hello do
-    IO.puts("Test2")
+  
+  def main do
+    x = 5
+    IO.puts(@x)
   end
 end
